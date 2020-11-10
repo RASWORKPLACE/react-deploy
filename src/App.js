a3b9d9d379ed75d21b8b5846866d56a7e21572bd
RASWORKPLACE <74257912+RASWORKPLACE@users.noreply.github.com>
2020-11-11 02:43:14 +0530

    first commit to deploy in github

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import profileimage from './img/saibaba.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> my react appp </h1>
+<h2> author: venkatesh</h2>
+<img src={profileimage} alt="profile image "/>
+
+    </div>
+  );
+}
+
+export default App;
